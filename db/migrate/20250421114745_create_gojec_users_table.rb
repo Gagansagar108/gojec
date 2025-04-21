7007b7f5a6e8410d3be4ba124b5af2be80e59ff3
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-21 17:21:05 +0530

    schema

--- db/migrate/20250421114745_create_gojec_users_table.rb
+++ db/migrate/20250421114745_create_gojec_users_table.rb
@@ -1,6 +1,26 @@
 class CreateGojecUsersTable < ActiveRecord::Migration[8.0]
   def change
-    create_table :gojec_users_tables do |t|
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :encrypted_password
+      t.boolean :status
+      t.timestamps
+    end
+
+    create_table :user_sessions do |t|
+      t.references :user
+      t.string :key 
+      t.boolean :status
+      t.timestamps
+    end
+
+  
+    create_table :payments do |t|
+      t.references :user
+      t.integer :provider 
+      t.integer :status
+      t.string :order_id
       t.timestamps
     end
   end
